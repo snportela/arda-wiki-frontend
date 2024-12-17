c61afe6b7ddc5d10906dec33b650429ac117bfc9
Sophia Portela <sophianobreportela@gmail.com>
2024-12-17 15:13:53 -0300

    password encryption - wip

--- js/search.js
+++ js/search.js
@@ -1,11 +1,9 @@
 const searchInput = document.querySelector(".search");
 const search = document.querySelector(".search-btn");
 
-function submit() {
-  if (searchInput) {
-    searchInput.addEventListener("input", () => {
-      const value = searchInput.value;
-      search.href = "/results.html?search=" + value;
-    });
-  }
+if (searchInput) {
+  searchInput.addEventListener("input", () => {
+    const value = searchInput.value;
+    search.href = "/results.html?search=" + value;
+  });
 }
